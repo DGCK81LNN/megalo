1cb6305c352fd6dbb85cab374b4b13d183688813
DGCK81LNN <triluolnn@163.com>
2021-12-12 18:41:55 +0800

    feat: progress bar

--- script.js
+++ script.js
@@ -27,22 +27,30 @@ var start = 0
 var aub = null
 
 file.onchange = () => {
+  file.disabled = true
   generateBtn.disabled = true
+  progress.removeAttribute('value')
   file.files[0].arrayBuffer()
     .then(/** @returns {Promise<AudioBuffer>} */
           (arrayBuffer) => new Promise(
             (res, rej) => audioContext.decodeAudioData(arrayBuffer, res, rej)
           ))
     .then(_aub => {
+      file.disabled = false
       generateBtn.disabled = false
+      progress.value = 1
       aub = _aub
     }, e => {
+      file.disabled = false
+      progress.value = 0
       alert(e)
     })
 }
 
 codebox.value = JSON.stringify({ qlen, qseq, mlenq, start }, null, 1).replace(/\n[ ]*/g, ' ')
-function generate() {
+async function generate() {
+  generateBtn.disabled = true
+  progress.removeAttribute('value')
   try {
     ({ qlen, qseq, mlenq, start } = JSON.parse(codebox.value))
     var channels = [];
@@ -56,6 +64,8 @@ function generate() {
 
     var dur2 = start + (aub.duration -start) / mlenq * qseq.length
     var aub2 = audioContext.createBuffer(channelCount, 0| sampleRate * dur2, sampleRate);
+
+    var t = Date.now()
 
     /** start time of current measure in source, in samples */
     var isample = sampleRate * start
@@ -72,16 +82,29 @@ function generate() {
           let subarr = channel.subarray(0| qps, 0| qps + qlens)
           aub2.copyToChannel(subarr, ci, qis)
         })
+        progress.value = isample / aub2.length
       })
       isample += qlens * qseq.length
       psample += qlens * mlenq
+
+      var _t = Date.now()
+      if (_t - t > 40) {
+        t = _t
+        await new Promise(r => setTimeout(r, 10))
+      }
     }
 
+    progress.removeAttribute('value')
+    await new Promise(r => setTimeout(r, 10))
+    // TODO: make audioBufferToWav() async
     var blob = new Blob([audioBufferToWav(aub2)], { type: "audio/wave" })
+    progress.value = 1
     audioEl.src = URL.createObjectURL(blob)
-    alert('ok')
   } catch (e) {
     alert(e)
+    progress.value = 0
+  } finally {
+    generateBtn.disabled = false
   }
 }
 
